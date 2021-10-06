d868462e878c125f5c67d2fe7dce8075864de8fe
mashaLutskevich2002 <57455346+mashaLutskevich2002@users.noreply.github.com>
2021-10-06 19:04:20 +0300

    change smth styles

--- src/components/UI/About/CreateAbout.jsx
+++ src/components/UI/About/CreateAbout.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./CreateAbout.module.css";
 import img from "../../../img/post.gif";
 import smile from "../../../img/smile.png";
+import CreateLoader from "../Loader/CreateLoader";
 
 const CreateAbout = () => {
+    const [content, setContent] = useState(false)
+
+    useEffect(()=>{
+        setContent(true)
+    })
+
     return (
         <div className={classes.main}>
-            <img src={img} alt='gif' className={classes.gif}/>
-            <h1 className={classes.text}>Веб-приложение для постов</h1>
-            <img className={classes.smile} src={smile} alt='smile' />
+            {
+                content
+                ? (
+                    <React.Fragment>
+                        <img src={img} alt='gif' className={classes.gif}/>
+                        <h1 className={classes.text}>Веб-приложение для постов</h1>
+                        <img className={classes.smile} src={smile} alt='smile' />
+                    </React.Fragment>
+                    )
+                : <CreateLoader/>
+            }
         </div>
     );
 };
